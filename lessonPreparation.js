793df23a97c87fc1129aa9a9962e3dc2d8b74fd6
Salavat <salavat.islamov.96@gmail.com>
2023-12-10 20:24:05 +0100

    Lesson preperation

---
+++ lessonPreparation.js
@@ -0,0 +1,30 @@
+// data types - типы данных - маалымат типтери
+// Number - число - сан
+let someNumber = 12;
+let anotherNumber = 14.5;
+let sumOfTwoNumbers = someNumber + anotherNumber;
+console.log(sumOfTwoNumbers);
+let infinityNumber = 1 / 0;
+console.log(infinityNumber); // Infinity - бесконечность - чексиз сан
+console.log("Aibek" * 2); // NaN - Not a Number - сан эмес
+// typeof оператору
+console.log(typeof someNumber);
+console.log(typeof anotherNumber);
+
+// BigInt - чон сан - большое число
+let money = 123123123123123456n;
+console.log(typeof money);
+
+// String - сап - строки
+let str = "Nurbek";
+let str2 = "Bul duino keremet!";
+let phrase = `Menin atym ${str}`;
+let str3 = str + str2;
+console.log(str3);
+
+//Boolean - логикалык тип
+let nikeluu = true;
+let baktyluu = true;
+let jaman = false;
+let chonby = 4 > 1;
+console.log(chonby);
---
+++ slideExamples.js
@@ -0,0 +1,44 @@
+let message;
+
+message = "Салам!";
+
+message = "Дуйно!";
+
+console.log(message);
+
+let meninBirichiMugalimim = "Гапыр агай";
+
+let secondName = "Asanov",
+  nationality = "kyrgyz";
+
+const myBirthDay = "01.01.2000";
+myBirthDay = "14.11.2001";
+
+let n = 123; // Бутун сан
+n = 12.345; // Болчок сан
+
+// Арттагы "n" белгиси сандын BigInt экенин аныктайт
+const bigInt = 1234567890123456789012345678901234567890n;
+
+let str = "Салам";
+let str2 = 'Бир тырмакча дагы колдонулат';
+let phrase = `башкаларын дагы камтый алат ${str}`;
+
+let nikeluu = true; // ооба, ал никелуу
+let deputat = false; // жок, ал депутат эмес
+let isGreater = 4 > 1; // ооба, 4 1ден чон
+
+// Атайын аныкталган бош маани
+let age = null;
+
+// Undefined - мааниси аныкталган эмес
+let dateOfBirth;
+
+let variable = "Кыргыз тили";
+const million = 1000000;
+let someVariable = 213; // Сан
+someVariable = "Бишкек" // Сап
+
+// Java тилиндеги статикалык типтештируу
+int numericVariable = 323
+
